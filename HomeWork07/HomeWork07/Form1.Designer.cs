d8ff19a6d04ab6a3ea1a867d6be2ae6ee6e10f9a
AllaMoskalenko <37500333+AllaMoskalenko@users.noreply.github.com>
2022-09-07 23:09:08 +0300

    Add files via upload

---
+++ HomeWork07/HomeWork07/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace HomeWork07
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.readFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeFileNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearWindowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Text1.txt",
+            "MyFile1.txt",
+            "NewFile1.txt",
+            "T1.txt",
+            "F1.txt"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 117);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Visible = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Text2.txt",
+            "MyFile2.txt",
+            "NewFile2.txt",
+            "T2.txt",
+            "F2.txt"});
+            this.comboBox2.Location = new System.Drawing.Point(365, 117);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Text3.txt",
+            "MyFile3.txt",
+            "NewFile3.txt",
+            "T3.txt",
+            "F3.txt"});
+            this.comboBox3.Location = new System.Drawing.Point(594, 117);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(137, 159);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(121, 150);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(365, 159);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(121, 150);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(594, 159);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(121, 150);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.readFileToolStripMenuItem,
+            this.changeFileNameToolStripMenuItem,
+            this.clearWindowsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // readFileToolStripMenuItem
+            // 
+            this.readFileToolStripMenuItem.Name = "readFileToolStripMenuItem";
+            this.readFileToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.readFileToolStripMenuItem.Text = "Read file";
+            this.readFileToolStripMenuItem.Click += new System.EventHandler(this.readFileToolStripMenuItem_Click);
+            // 
+            // changeFileNameToolStripMenuItem
+            // 
+            this.changeFileNameToolStripMenuItem.Name = "changeFileNameToolStripMenuItem";
+            this.changeFileNameToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.changeFileNameToolStripMenuItem.Text = "Change file name";
+            this.changeFileNameToolStripMenuItem.Click += new System.EventHandler(this.changeFileNameToolStripMenuItem_Click);
+            // 
+            // clearWindowsToolStripMenuItem
+            // 
+            this.clearWindowsToolStripMenuItem.Name = "clearWindowsToolStripMenuItem";
+            this.clearWindowsToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.clearWindowsToolStripMenuItem.Text = "Clear windows";
+            this.clearWindowsToolStripMenuItem.Click += new System.EventHandler(this.clearWindowsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(164, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(139, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(365, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(594, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(246, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "New file name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem readFileToolStripMenuItem;
+        private ToolStripMenuItem changeFileNameToolStripMenuItem;
+        private ToolStripMenuItem clearWindowsToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ HomeWork07/HomeWork07/Form1.cs
@@ -0,0 +1,104 @@
+using System.IO;
+
+namespace HomeWork07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+
+        }
+        string file1, file2="Text2.txt", file3="Text3.txt", str;
+
+        private void changeFileNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string newname2, newname3;    
+            
+            if (comboBox2.SelectedItem != null)
+            {
+                newname2 = comboBox2.SelectedItem.ToString();
+                FileInfo fileInfo2 = new FileInfo(file2);
+                System.IO.File.Move(fileInfo2.Name,newname2);
+                label2.Text = newname2;
+                file2 = newname2;
+            }
+
+            if (comboBox3.SelectedItem != null)
+            {
+                newname3 = comboBox3.SelectedItem.ToString();
+                FileInfo fileInfo3 = new FileInfo(file3);
+                System.IO.File.Move(fileInfo3.Name, newname3);
+                label3.Text = newname3;
+                file3 = newname3;
+            }
+            
+            
+        }
+
+        private void clearWindowsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        private void readFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+            file1 = System.IO.Path.GetFullPath("Text1.txt"); //file for reading
+            FileInfo fileInfo1 = new FileInfo(file1);    
+            label1.Text = fileInfo1.Name;
+            FileStream fileStream1 = File.OpenRead(file1);
+            StreamReader reader = new StreamReader(fileStream1);
+            
+            //file2 = "Text2.txt";                             //first file for writing
+            label2.Text = file2;
+            FileStream fileStream2 = File.Create(file2);
+            StreamWriter streamWriter2 = new StreamWriter(fileStream2);
+
+            
+            //file3 = "Text3.txt";                            //second file for writing
+            label3.Text = file3;
+            FileStream fileStream3 = File.Create(file3);
+            StreamWriter streamWriter3 = new StreamWriter(fileStream3);
+            //reading lines
+
+            int c = 0;
+            while (reader.Peek() >= 0)
+            {
+                str = reader.ReadLine();
+                c++;
+                richTextBox1.Text += str + "\n";
+                if (c%2 == 0)
+                {
+                    streamWriter2.WriteLine(str);
+                    richTextBox2.Text += str + "\n";
+                }
+                else
+                {
+                    streamWriter3.WriteLine(str);
+                    richTextBox3.Text += str + "\n";
+                }
+            }
+            reader.Close();
+            streamWriter2.Close();
+            streamWriter3.Close();
+
+
+        }
+    }
+}
