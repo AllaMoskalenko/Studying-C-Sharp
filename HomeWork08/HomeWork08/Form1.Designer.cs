a2d5b7afda7a8d7b1cf120093f092e43f54dc314
AllaMoskalenko <37500333+AllaMoskalenko@users.noreply.github.com>
2022-09-08 21:29:52 +0300

    Add files via upload

---
+++ HomeWork08/HomeWork08/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HomeWork08
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(140, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(425, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(24, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(110, 373);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "X";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 39;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Y";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 39;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(24, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Build";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(84, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(583, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ HomeWork08/HomeWork08/Form1.cs
@@ -0,0 +1,84 @@
+namespace HomeWork08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ourfile, f1, str;
+            string[] coord;
+            int c = 0;
+            
+            pictureBox1.Refresh();
+            dataGridView1.Rows.Clear();
+            
+            ourfile = "Coordinates.txt";
+            f1 = System.IO.Path.GetFullPath(ourfile);
+            FileStream fs = File.OpenRead(f1);
+            StreamReader sr = new StreamReader(f1);
+
+            while (sr.Peek() != -1) 
+            {
+                str = sr.ReadLine();
+                coord = str.Split(" ");
+                dataGridView1.Rows.Add(Convert.ToInt32(coord[0]), Convert.ToInt32(coord[1]));
+                c++;
+            }
+            sr.Close();
+            fs.Close();
+            
+            //axes
+            int x0 = 0, y0 = 0, x, y;
+            x = pictureBox1.Width / 2;
+            y = pictureBox1.Height / 2;
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen1 = new Pen(Color.Black, 1);
+            graphics.DrawLine(pen1, x0, y, 2*x, y);
+            graphics.DrawLine(pen1, x, y0, x, 2*y);
+
+            //lines
+            Pen pen2 = new Pen(Color.Blue, 2);
+            for (int i = 0; i < c-1; i++)
+            {
+                graphics.DrawLine(pen2, Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value) + x,
+                                        y - Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value),
+                                        Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[0].Value) + x,
+                                        y - Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[1].Value) ) ;
+            }
+            
+            //find length of lines
+            double[] len = new double[c-1];
+            for (int i = 0; i < c - 1; i++)
+            {
+                len[i] = Math.Sqrt(Math.Pow((Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[0].Value) - Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value)), 2) 
+                                 + Math.Pow((Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value) - Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[1].Value)), 2));            
+            }
+            double maxlen = len[0];
+            int ind = 0;
+            for (int i = 1; i < len.Length; i++)
+            {
+                if(len[i] > maxlen) 
+                { 
+                    maxlen = len[i]; 
+                    ind = i;
+                }                  
+            }
+            
+            //drew max line another color
+            Pen pen3 = new Pen(Color.Aquamarine, 3);
+            graphics.DrawLine(pen3, Convert.ToInt32(dataGridView1.Rows[ind].Cells[0].Value) + x,
+                                    y - Convert.ToInt32(dataGridView1.Rows[ind].Cells[1].Value),
+                                    Convert.ToInt32(dataGridView1.Rows[ind + 1].Cells[0].Value) + x,
+                                    y - Convert.ToInt32(dataGridView1.Rows[ind + 1].Cells[1].Value));
+        }
+    }
+}
