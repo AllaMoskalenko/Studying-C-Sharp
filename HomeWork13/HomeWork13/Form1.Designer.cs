066155779676a1e68daa8f6124c357ae9968398a
AllaMoskalenko <37500333+AllaMoskalenko@users.noreply.github.com>
2022-09-13 20:54:16 +0300

    Add files via upload

---
+++ HomeWork13/HomeWork13/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace HomeWork13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 33);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(23, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(157, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(447, 294);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 339);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ HomeWork13/HomeWork13/Form1.cs
@@ -0,0 +1,92 @@
+namespace HomeWork13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k, h, w, fh, fw, marker;
+            k = Convert.ToInt32(textBox1.Text);
+            Point point1 = new Point(0 , 0);
+            Point point2 = new Point(k , k);
+            Pen pen1 = new Pen( Color.Blue, 2);
+            Graphics g = pictureBox1.CreateGraphics();
+
+            h = pictureBox1.Height;
+            w = pictureBox1.Width;
+
+            fh = 1;
+            fw = 1;
+            marker = 1;
+
+            //risuyu
+            while (true)
+            {
+                if (marker == 1)
+                {
+                    g.DrawLine(pen1, point1, point2);
+                    marker = 0;
+                }
+                else marker = 1;
+
+                point1.X = point2.X;
+                point1.Y = point2.Y;
+                point2.X += k * fw;
+                point2.Y += k * fh;
+                if (point2.X > w && point2.Y >h)
+                {
+                    break;
+                }
+                if (point2.X < 0 && point2.Y > h)
+                {
+                    break;
+                }
+
+                if (point2.X > w && point2.Y < 0)
+                {
+                    break;
+                }
+                if (point2.X < 0 && point2.Y < 0)
+                {
+                    break;
+                }
+                if (point2.Y > h)    //proverka dostizheniya nizhney granitcy
+                {
+                    fh = -1;
+                    point2.Y += 2 * k * fh;
+                }
+                if (point2.X > w)       //proverka dostizheniya pravoy granitcy
+                {
+                    fw = -1;
+                    point2.X += 2 * k * fw;
+                }
+                if (point2.Y < 0)
+                {
+                    fh = 1;
+                    point2.Y += 2 * k * fh;
+                }
+                if (point2.X < 0)
+                {
+                    fw = 1;
+                    point2.X += 2 * k * fw;
+                }
+            }
+
+        }
+    }
+}
